958cd46600e8da2426dc079c54326951e3592627
scarletgv <scarletgv@hotmail.com>
2021-02-25 22:27:38 -0300

    Novo Projeto: Atualizando o Template

--- lists/tests.py
+++ lists/tests.py
@@ -42,6 +42,12 @@ class ListViewTest(TestCase):
         my_list = List.objects.create()
         response = self.client.get(f'/lists/{my_list.id}/')
         self.assertTemplateUsed(response, 'list.html')
+    
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
